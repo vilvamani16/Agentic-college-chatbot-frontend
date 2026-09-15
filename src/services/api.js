0bc9155cbb3ae482b2985e98cf7a4dc20a742684
Vilvamani <vilvamani145@gmail.com>
2026-09-15 11:30:28 +0530

    Configure production backend URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: `${import.meta.env.BACKEND_URL}/api`,
 });
 
 API.interceptors.request.use((config) => {
